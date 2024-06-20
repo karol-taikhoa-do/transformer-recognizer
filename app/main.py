f749da0e63b80408db3ff8c20508c97c0cb38584
karol-taikhoa-do <dotaikhoa@gmail.com>
2024-06-20 11:45:18 +0200

    working annotations models' setup

--- app/main.py
+++ app/main.py
@@ -1,58 +1,91 @@
 import streamlit as st
-from typing import Tuple
 from PIL import Image
-import cv2
 from PIL.Image import Image as PImage
 from datetime import datetime
 
+IMAGA_NAME='img.png'
+IMAGE_DIR="images"
+WORKING_IMAGE_DIR="runs/detect/predict"
+FINAL_IMAGE_DIR="runs/detect/predict2"
+CAR_MODEL_PATH="./model/car.pt"
+TRANSFORMER_MODEL_PATH="./model/transformer.pt"
+
 MAX_FILE_SIZE = 200 * 1024 * 1024  # 5MB
+
 
 @st.cache_resource
 def load_models():
+    """
+    laduje przy pierwszym uruchomieniu modele i zapisuje w cache
+    programu dla pozniejszego szybszego dzialania.
+    Usuwa poprzednie pliki lub przykladowe dla modeli (runs)
+    dla poprawnego dzialania funkcji przetwarzania obrazu
+    """
     from ultralytics import YOLO
-    car_model = YOLO("./model/car.pt")
-    transformer_model = YOLO("./model/transformer.pt")
+    import shutil
+    import os
+
+    path = os.getcwd()
+    shutil.rmtree(str(path) + "\\runs")
+
+    car_model = YOLO(CAR_MODEL_PATH)
+    transformer_model = YOLO(TRANSFORMER_MODEL_PATH)
     models = [car_model, transformer_model]
     return models
 
-def rectangle_on_image(image, result, model) -> None:
-    boxes = result.boxes.cpu().numpy()  # Get boxes on CPU in numpy format
-    for box in boxes:  # Iterate over boxes
-        r = box.xyxy[0].astype(int)  # Get corner points as int
-        class_id = int(box.cls[0])  # Get class ID
-        class_name = model.names[class_id]  # Get class name using the class ID
-        print(f"Class: {class_name}, Box: {r}")  # Print class name and box coordinates
-        cv2.rectangle(image, r[:2], r[2:], (0, 255, 0), 2)  # Draw boxes on the image
 
-
-def model_annotate(model, image: PImage) -> Tuple[PImage,str]:
-    result = model(source=image, save=True)
-    # processed_image = cv2.imread('.png')
+def model_annotate(model) -> str:
+    """
+    model przetwarza zdjecie z pliku i wynik detekcji nanosi na 
+    osobne zdjecie (nietypowa wlasnosc funkcji 'predict', 
+    nie widalem opcji custmizowanie WORKING_DIR).
+    Wynikowe zdjecie zapisuje jako plik do dalszego odczytu,
+    a funkcja zwraca wynik detekcji jako string 
+    """
+    result = model.predict(f"{IMAGE_DIR}/{IMAGA_NAME}", save=True,conf=0.5)
+    annoted_image = Image.open(f"{WORKING_IMAGE_DIR}/{IMAGA_NAME}")
     detection_count = 0
+  
     for res in result:
         detection_count += len(res.boxes.conf)
+        res.show()
+
+    annoted_image.save(f"{IMAGE_DIR}/{IMAGA_NAME}")
     
     if detection_count:
-            return image,f"{detection_count} {result[0].names[int(result[0].boxes.cls.data[0])]} found"
+        res = f" {detection_count} {result[0].names[int(result[0].boxes.cls.data[0])]}"
+        return res+"s" if detection_count > 1 else res
     
-    return image,"No detections"
+    return ""
         
 
-def process_image(img: PImage) -> Tuple[PImage, str]:
-    """ model recognize """
-    models = load_models()
+def process_image() -> str:
+    """
+    uruchamia wszystkie modele i zapisuje skumulowane wyniki detekcji
+    jako string. Jesli nic nie wykryly modele, zwroc odpowiednie info
+    """
+
+    detections = []
 
     for model in models:
-        boxed_img, detections = model_annotate(model, img)
+        print(model.model_name)
+        cur_detections = model_annotate(model)
+
+        if len(cur_detections):
+            detections.append(cur_detections)
 
     if len(detections):
-        return boxed_img, detections
+        return ','.join(detections)+" found"
     
-    return img, "no detections"
+    return "No detections"
 
 
-def display_images(original_image, processed_image, detections: str) -> None:
-
+def display_images(original_image: PImage, processed_image: PImage, detections: str) -> None:
+    """
+    uzywa kolumn do wyswietlenia obrazow orginal, przetworzony przez modele i
+    slowne podsumowanie skumulowanej deteckji przez modele.
+    Ustawia na koniec przycisk na pobranie wynikowego zdjecia
+    """
     col1.empty()
     col2.empty()
     with col1:
@@ -77,9 +110,9 @@ def display_images(original_image, processed_image, detections: str) -> None:
         mime="image/png"
     )   
 
-
+# MAIN
 st.set_page_config(layout="wide", page_title="Transformer recognizer")
-
+models = load_models()
 st.write("## Transformer recognizer")
 st.write(
 "Upload image to see if there is a Transformer, car that has been Transformer's cover or just an ordinary car."
@@ -93,7 +126,9 @@ my_upload = st.sidebar.file_uploader("Upload an image", type=["PNG"])
 
 if img_buff is not None:
     image = Image.open(img_buff)
-    processed_image, detections = process_image(image)
+    image.save(f"{IMAGE_DIR}/{IMAGA_NAME}")
+    detections = process_image()
+    processed_image = Image.open(f"{FINAL_IMAGE_DIR}/{IMAGA_NAME}")
     display_images(image, processed_image, detections)
 
 if my_upload is not None:
@@ -102,7 +137,9 @@ if my_upload is not None:
         st.error("The uploaded file is too large. Please upload an image smaller than 200MB.")
     else:
         image = Image.open(my_upload)
-        processed_image, detections = process_image(image)
+        image.save(f"{IMAGE_DIR}/{IMAGA_NAME}")
+        detections = process_image()
+        processed_image = Image.open(f"{FINAL_IMAGE_DIR}/{IMAGA_NAME}")
         display_images(image, processed_image, detections)
 
   
